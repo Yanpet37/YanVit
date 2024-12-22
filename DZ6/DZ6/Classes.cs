01b9a3f1071451561f16f5e5067ec0ecbcb1a9e4
y.piatrouski <y.piatrouski@gmail.com>
2024-12-22 18:47:05 +0300

    1 - created abstract class and subclasses 2 - cteated static class 3 - created program

---
+++ DZ6/DZ6/Classes.cs
@@ -0,0 +1,45 @@
+﻿using System.Xml.Linq;
+
+namespace Classes
+{
+    public abstract class EmployeeBase
+    {
+        public string Name { get; set; }
+
+        public string Position { get; set; }
+        public abstract string GetDetails();
+    }
+
+    class Manager : EmployeeBase
+    {
+        private string _grade; 
+        public string Grade 
+        {
+            get => _grade; 
+            set
+            {
+                if (value != "J" && value != "M" && value != "S")
+                {
+                    throw new ArgumentException("Grade must be J, M, or S.");
+                }
+                _grade = value; 
+            }
+        }
+
+
+        public override string GetDetails()
+        {
+            return $"Manager Name: {Name}, Position: {Position}, Grade: {Grade}";
+        }
+    }
+    class Worker : EmployeeBase
+    {
+        public string Location { get; set; }
+
+        public override string GetDetails()
+        {
+            return $"Worker Name: {Name}, Position: {Position}, Location: {Location}";
+        }
+    }
+}
+
---
+++ DZ6/DZ6/Static.cs
@@ -0,0 +1,111 @@
+﻿using System; 
+using System.Collections.Generic;
+using Classes;
+
+namespace Static
+{
+    static class EmployeeRegistry
+    {
+        private static List<EmployeeBase> employees = new List<EmployeeBase>();
+        public static void AddEmployee()
+        { 
+            while (true)
+        {
+            Console.WriteLine("Choose the type of employee to add:");
+            Console.WriteLine("1. Manager");
+            Console.WriteLine("2. Worker");
+            Console.Write("Enter your choice: ");
+            string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        Console.Write("Enter Name: ");
+                        string nameManager = Console.ReadLine();
+                        Console.Write("Enter Position: ");
+                        string positionManager = Console.ReadLine();
+                        Console.Write("Enter Grade (J, M, or S): ");
+                        string grade;
+                        while (true)
+                        {
+                            grade = Console.ReadLine();
+                            if (grade == "J" || grade == "M" || grade == "S")
+                            {
+                                break;
+                            }
+                            Console.WriteLine("Grade must be J, M, or S. Please try again.");
+                        }
+
+                        Manager manager = new Manager
+                        {
+                            Name = nameManager,
+                            Position = positionManager,
+                            Grade = grade,
+                        };
+
+                        employees.Add(manager);
+                        Console.WriteLine($"Manager {manager.Name} added.\n");
+                        return;
+
+                    case "2":
+                        Console.Write("Enter Name: ");
+                        string nameWorker = Console.ReadLine();
+                        Console.Write("Enter Position: ");
+                        string positionWorker = Console.ReadLine();
+                        Console.Write("Enter Location: ");
+                        string location = Console.ReadLine();
+
+                        Worker worker = new Worker
+                        {
+                            Name = nameWorker,
+                            Position = positionWorker,
+                            Location = location,
+                        };
+
+                        employees.Add(worker);
+                        Console.WriteLine($"Worker {worker.Name} added.\n");
+                        return;
+
+                    default:
+                        Console.WriteLine("\"Invalid choice. Please enter a valid number (1 or 2).\\n\"");
+                        break;
+                }
+            }
+        }
+
+
+        public static void ListAllEmployees()
+        {
+            if (employees.Count == 0)
+            {
+                Console.WriteLine("The employee list is empty.\n");
+                return;
+            }
+
+            Console.WriteLine("List of all employees:\n");
+            foreach (var employee in employees)
+            {
+                Console.WriteLine(employee.GetDetails());
+            }
+        }
+        public static void FindEmployee()
+        {
+            Console.WriteLine("Enter the name of the employee:");
+            string name = Console.ReadLine();
+
+            var matchingEmployees = employees.FindAll(e => e.Name == name);
+            if (matchingEmployees.Count > 0)
+            {
+                Console.WriteLine($"Found {matchingEmployees.Count} employee(s):");
+                foreach (var employee in matchingEmployees)
+                {
+                    Console.WriteLine(employee.GetDetails()); 
+                }
+            }
+            else
+            {
+                Console.WriteLine("Employee not found.");
+            }
+        }
+    }
+}
