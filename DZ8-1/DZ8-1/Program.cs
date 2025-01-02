28328fe1417868b9b901849407069778de3ac5c6
y.piatrouski <y.piatrouski@gmail.com>
2025-01-02 22:01:47 +0300

    Completed the second task

---
+++ DZ8-1/DZ8-1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace DZ8_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите слово!");
+            string slovo = Console.ReadLine();
+
+            string slovo2 = slovo.Replace(" ", "");
+
+            char[] charSlovo = slovo2.ToCharArray();
+            Array.Reverse(charSlovo);
+            string slovo3 = new string(charSlovo);
+
+            if (slovo2.Equals(slovo3, StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine("Слово является палиндромом");
+            }
+            else
+            {
+                Console.WriteLine("Слово не является палиндромом");
+            }
+        }
+    }
+}
