856546126d05622839e85779701b8688c9105bfc
y.piatrouski <y.piatrouski@gmail.com>
2025-01-11 22:38:12 +0300

    Сделал вторую задачу

---
+++ DZ8-2/DZ8-2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+namespace DZ8_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "Helo World";
+            StringBuilder sb = new StringBuilder();
+
+            foreach (char element in input)
+            {
+                if (element != ' ')
+                {
+                    sb.Append(element);
+                }
+            }
+            string result = sb.ToString();
+            Console.WriteLine($"Результат: {result}");
+        }
+    }
+}
