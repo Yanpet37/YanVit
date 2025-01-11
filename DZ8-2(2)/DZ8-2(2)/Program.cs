833e4fcc409a6146b817be68be8231594cefb93b
y.piatrouski <y.piatrouski@gmail.com>
2025-01-11 22:45:57 +0300

    Completed the second task one more time (found better aproach)

---
+++ DZ8-2(2)/DZ8-2(2)/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+
+namespace DZ8_2_2_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = ("Hello World");
+            StringBuilder sb = new StringBuilder("Hello World");
+            sb.Replace(" ", "");
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
