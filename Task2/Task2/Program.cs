533229ece0e214ea6086242d1604f69f6609f5c2
y.piatrouski <y.piatrouski@gmail.com>
2025-01-15 14:02:02 +0300

    Task 2

---
+++ Task2/Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Task2
+{
+    public class MyString
+    {
+        private string value;
+
+        public MyString()
+        {
+            value = string.Empty;
+        }
+
+        public MyString(string input)
+        {
+            value = input;
+        }
+
+        public string Reverse()
+        {
+            char[] charArray = value.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
