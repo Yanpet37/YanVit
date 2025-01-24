316c82beff673409002ef8db475d34af42f284af
y.piatrouski <y.piatrouski@gmail.com>
2025-01-24 22:29:15 +0300

    1 task

---
+++ DZ9-1/DZ9-1/Prog.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Student> students = new List<Student>
+        {
+            new Student("Ян", 29, 7),
+            new Student("Сергей", 30, 8),
+            new Student("Марина", 25, 5),
+        };
+        students.Sort((s1, s2) => s2.Score.CompareTo(s1.Score));
+        Console.WriteLine("Список студентов по среднему баллу:");
+        foreach (var student in students)
+        {
+            student.Print();
+        }
+        students.Sort((s1, s2) => s1.Age.CompareTo(s2.Age));
+        Student youngest = students[0];
+        Console.WriteLine("Самый младший студент:");
+        youngest.Print();
+    }
+}
