47f8e67e2de46e5b3056bbd229733cea9d61e4a8
y.piatrouski <y.piatrouski@gmail.com>
2025-02-16 21:20:19 +0300

    task 2 is completed

---
+++ KR2/KR2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+public static class IntExtensions
+{
+    public static string ToWords(this int number)
+    {
+        string[] digits = { "ноль", "один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять" };
+
+        StringBuilder result = new StringBuilder();
+
+        foreach (char digit in number.ToString())
+        {
+            result.Append(digits[digit - '0']).Append(" ");
+        }
+
+        return result.ToString().Trim();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        int num = 1234;
+        Console.WriteLine(num.ToWords()); 
+    }
+}
